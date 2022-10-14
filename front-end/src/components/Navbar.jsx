4c83abde2f572c59c5b79d98ab7ce41db5fa259d
Mohammed Jaseem Tp <56159302+Mohammedjaseem@users.noreply.github.com>
2022-10-14 20:07:48 +0530

    Admin & logout done

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -2,6 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import React from 'react';
+
+
+function logout() {
+  localStorage.clear();
+  sessionStorage.clear();
+  window.location.href = '/login';
+}
 
 
 function NavbarBootstrap() {
@@ -21,11 +30,19 @@ function NavbarBootstrap() {
           </Nav>
           <Link  to="/register"> <button className='btn btn-danger mx-3'> Register  </button></Link>
           <Link  to="/login"><button className='btn btn-info mx-3'> Login  </button></Link>
+        
+        
+           
+          {/* on button click logout */}
+          {/* <button className='btn btn-warning mx-3' onClick={logout}> Logout  </button> */}
+          <button className='btn btn-warning mx-3' onClick={()=> logout()}> Logout  </button>
+
           
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
+
 
 export default NavbarBootstrap;
